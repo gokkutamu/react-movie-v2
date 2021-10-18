afe078811e3e15fe2347c97cddcb93869ea66732
Ngoc Tam <68436770+TranNgocTam-19211TT2494@users.noreply.github.com>
2021-10-18 17:15:46 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import { Home } from "./components/Home/Home";
 import { Login } from "./components/Account/Login";
+import Search from "./components/Search/Search";
 export function App() {
   return (
     
@@ -10,6 +11,7 @@ export function App() {
       <Switch>
         <Route path="/" component={Home} exact />
         <Route path="/login" component={Login} exact />
+        <Route path="/search" component={Search} exact />
       </Switch>
     </main>
   );
