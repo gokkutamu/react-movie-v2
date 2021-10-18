6f07c1466fe580d713ceb5242bce572107bfdbdb
TranNgocTam <ngoctam2303001@gmail.com>
2021-10-18 14:04:15 +0700

    Up source code chinh

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import { Home } from "./components/Home/Home";
+import { Login } from "./components/Account/Login";
+export function App() {
+  return (
+    
+    <main>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/login" component={Login} exact />
+      </Switch>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Account/Login.jsx
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "./Login/Login.css";
+
+
+import {
+
+
+} from "../../server";
+
+export function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+
+        };
+        fetchAPI();
+    }, []);
+
+    function validateForm() {
+        return email.length > 0 && password.length > 0;
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+    }
+
+
+
+    return (
+
+        <div className="main-container">
+            <div className="header">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <nav>
+                                <ul className="menu">
+                                    <li className="nav-hover"><a href="/">Home</a></li>
+                                    <li className="nav-hover">
+                                        <div className="login-templeta">
+                                            <a href="#">Login</a>
+                                        </div>
+                                    </li>
+                                    <li className="nav-hover">
+                                        <div className="login-templeta">
+                                            <a href="#">Register</a>
+                                        </div>
+                                    </li>
+                                </ul>
+
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div className="Login">
+                <h2 className="name-login">Movie Login</h2>
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group size="lg" controlId="email">
+                        <Form.Label >Email</Form.Label>
+                        <Form.Control
+                            autoFocus
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </Form.Group>
+                    <Form.Group size="lg" controlId="password">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </Form.Group>
+                    <Button block size="lg" type="submit" disabled={!validateForm()}>
+                        Login
+                    </Button>
+                </Form>
+            </div>
+        </div>
+
+    );
+}
