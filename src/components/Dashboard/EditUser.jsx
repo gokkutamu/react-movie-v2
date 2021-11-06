4674a91fb63a385a7116719d45c81f5df643f61a
ngothanhthai297 <ngothai653@gmail.com>
2021-11-06 23:30:25 +0700

    Fix bug v2

--- src/components/Dashboard/EditUser.jsx
+++ src/components/Dashboard/EditUser.jsx
@@ -24,7 +24,7 @@ export function EditUser({ match }) {
 
     useEffect(() => {
         const fetchAPI = async () => {
-            setUserById(await userById(split(params.id)));
+            setUserById(await userById(params.id));
         };
         fetchAPI();
     }, []);
