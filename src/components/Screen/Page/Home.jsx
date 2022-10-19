580600d92a05d508358898673a00470466593a10
Tran Ngoc Tam <tran.ngoc.tam@uruca.jp>
2022-10-19 13:08:37 +0700

    fix

--- src/components/Screen/Page/Home.jsx
+++ src/components/Screen/Page/Home.jsx
@@ -86,46 +86,13 @@ const Home = () => {
                         </div>
                     </div>
                     {!selector && (
-                        <MovieList category={category.tv} type={watchingTVTypes.on_the_air} />
+                        <MovieList category={category.tv} type={watchingTVTypes.airing_today} />
                     )}
                     {selector === "wtv" && (
-                        <MovieList category={category.tv} type={watchingTVTypes.on_the_air} />
+                        <MovieList category={category.tv} type={watchingTVTypes.airing_today} />
                     )}
                     {selector === "film" && (
                         <MovieList category={category.movie} type={movieTypes.upcoming} />
-                    )}
-                </div>
-
-                <div className="section mb-3">
-                    <div className="section__header mb-2">
-                        <h2>Trending</h2>
-                        <div className="flex justify-between md:items-end items-center">
-                            <div className="inline-flex gap-[40px] pb-[5px] border-b border-gray-darken relative">
-                                <button className={`${currentTab === "tv" && "text-white font-medium after:absolute after:bottom-0 after:left-[10%] after:bg-white after:h-[3px] after:w-5"
-                                    } transition duration-300 hover:text-white`} onClick={() => {
-                                        setCurrentTab("tv");
-                                        localStorage.setItem("currentTab", "tv");
-                                    }}>
-                                    Today
-                                </button>
-                                <button className={`${currentTab === "movie" && "text-white font-medium after:absolute after:bottom-0 after:right-[9%] after:bg-white after:h-[3px] after:w-10"
-                                    } transition duration-300 hover:text-white`} onClick={() => {
-                                        setCurrentTab("movie");
-                                        localStorage.setItem("currentTab", "movie");
-                                    }}>
-                                    This Week
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    {!currentTab && (
-                        <MovieList category={category.movie} type={movieTypes.popular} />
-                    )}
-                    {currentTab === "movie" && (
-                        <MovieList category={category.movie} type={movieTypes.popular} />
-                    )}
-                    {currentTab === "tv" && (
-                        <MovieList category={category.tv} type={watchingTVTypes.popular} />
                     )}
                 </div>
             </div>
